98242c24cea52b414e5e3d43ce25a47fb7fc02eb
Samuel Venable <samuelvenable@hotmail.com>
2024-10-17 01:15:29 -0400

    Update debian-install-arm64.sh

--- debian-install-arm64.sh
+++ debian-install-arm64.sh
@@ -1,1 +1,1 @@
-sudo apt-get install git build-essential libxinerama-dev libxrandr-dev libxext-dev libxrender-dev libx11-dev
+sudo apt-get install git build-essential crossbuild-essential-arm64 libxinerama-dev:arm64 libxrandr-dev:arm64 libxext-dev:arm64 libxrender-dev:arm64 libx11-dev:arm64
