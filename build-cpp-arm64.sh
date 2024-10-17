0b91311af67a4cd668779a91f307c3fd7131f922
samuelvenable <samuelvenable@hotmail.com>
2024-10-17 01:26:28 -0400

    yay

--- build-cpp-arm64.sh
+++ build-cpp-arm64.sh
@@ -1,4 +1,4 @@
 cd "${0%/*}" 
-git clone git://github.molgen.mpg.de/git-mirror/glibc.git
+git clone https://github.molgen.mpg.de/git-mirror/glibc.git
 ./glibc/configure --enable-static-nss
 g++ -no-pie XDisplayGetters.cpp ExampleUsage.cpp -Wl,-Bstatic -static-libstdc++ -static-libgcc -lXinerama -lXrandr -lXext -lXrender -lX11 -lxcb -lXau -lXdmcp -Wl,-Bdynamic -ldl -o xgetdisplay-cpp-arm64 -fPIC
