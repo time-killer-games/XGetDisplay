8c1fc5378a11a0ad81ebd5419b8d082202eea655
samuelvenable <samuelvenable@hotmail.com>
2024-10-17 01:43:34 -0400

    yay

--- build-c-armhf.sh
+++ build-c-armhf.sh
@@ -1,4 +1,4 @@
 cd "${0%/*}" 
 git clone https://github.molgen.mpg.de/git-mirror/glibc.git
 ./glibc/configure --enable-static-nss
-gcc -no-pie XDisplayGetters.c ExampleUsage.c -Wl,-Bstatic -static-libstdc++ -static-libgcc -lXinerama -lXrandr -lXext -lXrender -lX11 -lxcb -lXau -lXdmcp -Wl,-Bdynamic -ldl -o xgetdisplay-c-arm64 -fPIC
+gcc -no-pie XDisplayGetters.c ExampleUsage.c -Wl,-Bstatic -static-libstdc++ -static-libgcc -lXinerama -lXrandr -lXext -lXrender -lX11 -lxcb -lXau -lXdmcp -Wl,-Bdynamic -ldl -o xgetdisplay-c-armhf -fPIC
