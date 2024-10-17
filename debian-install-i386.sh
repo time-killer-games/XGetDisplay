85becc68c387c485c760cac1497f91b4336759a1
Samuel Venable <samuelvenable@hotmail.com>
2024-10-17 01:14:55 -0400

    Update debian-install-i386.sh

--- debian-install-i386.sh
+++ debian-install-i386.sh
@@ -1,1 +1,1 @@
-sudo apt-get install git build-essential libxinerama-dev libxrandr-dev libxext-dev libxrender-dev libx11-dev
+sudo apt-get install git build-essential crossbuild-essential-i386 libxinerama-dev:i386 libxrandr-dev:i386 libxext-dev:i386 libxrender-dev:i386 libx11-dev:i386
